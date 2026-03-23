e74a6ae7680c0d26c221c0764e9feb0a7523d0df
shinethit <khunthanshwe@gmail.com>
2026-03-23 09:42:01 +0630

    Bug Fix

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'cherry-dir-v3'
-const STATIC_CACHE = 'cherry-static-v3'
+const CACHE_NAME = 'cherry-dir-v4'
+const STATIC_CACHE = 'cherry-static-v4'
 
 // Static assets to pre-cache
 const PRECACHE_URLS = [
